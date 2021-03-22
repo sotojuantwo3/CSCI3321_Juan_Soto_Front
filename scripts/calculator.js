728f11e5752f65350f135e1341595d16bb5ceba6
sotojuantwo3 <77467455+sotojuantwo3@users.noreply.github.com>
2021-03-22 14:53:01 -0500

    Added the finshing code to complete the bonus cred

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,3 +1,5 @@
+var clicked = false; // indicator if the equal button has been already clicked
+
 function updateDisplay(newValue){
     //Find display box
     var d = document.getElementById('display');
@@ -13,47 +15,58 @@ function updateDisplay(newValue){
         case '7':
         case '8':
         case '9':
-            d.innerHTML += newValue;
+            // if the display is displaying "Err" or the equal button already clicked
+                // newValue is displayed only
+            // else
+                // newValue is added onto display
+            if(d.innerHTML === "Err" || clicked){
+                d.innerHTML = newValue;
+                clicked = false;
+                break;
+            }
+            else{
+                d.innerHTML += newValue;
+            }
             break;
-
         case 'AC':
             d.innerHTML = '';
             break;
-
         case '+':
         case '-':
         case '*':
         case '/':
             // if the display box is empty
-            d.innerHTML += newValue;
-            break;
                 // do nothing
             // else if the display value ends with +,-,*,/
                 // do nothing            
             // else
                 // apend the key to the display
+            if(d.innerHTML === ''){
+                break;
+            }
+            else if(d.innerHTML.endsWith('+') || d.innerHTML.endsWith('-') || d.innerHTML.endsWith('*') || d.innerHTML.endsWith('/')){
+                break;
+            }
+            else{
+                d.innerHTML += newValue;
+            }
+            break;
 
         case '=':
             // if the display box is not empty
                 // call Javascript's evaluation funciton to evaluate
                 // the content of the display box
+            clicked = true;
+            if(d.innerHTML === ''){
                 break;
+            }
+            else if(d.innerHTML.endsWith('+') || d.innerHTML.endsWith('-') || d.innerHTML.endsWith('*') || d.innerHTML.endsWith('/')){
+                d.innerHTML = "Err";
+            }
+            else{
+                d.innerHTML = eval(d.innerHTML);
+            }
+            break;
     }
 
-}
-
-function add(x, y){
-    return x+y;
-}
-
-function subtract(x,y){
-    return x-y;
-}
-
-function multi(x,y){
-    return x*y;
-}
-
-function divide(x,y){
-    return x/y;
 }
